6911763663b5ca115d89948af75d925e9ac4b613
Emma Tola Laca <emmatola444@gmail.com>
2025-08-31 18:52:44 -0400

    Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
-import { RoomComponent } from './pages/room/room.component';
-import { ReservationComponent } from './pages/reservation/reservation.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 
 export const routes: Routes = [
-    {path: '', component: LayoutComponent},
-    {path: 'pages/room', component: RoomComponent},
-    {path: 'pages/reservation', component: ReservationComponent}
+    { path: '', redirectTo: 'pages', pathMatch: 'full'},
+    { 
+        path: 'pages', 
+        component: LayoutComponent,
+        loadChildren: () => import('./pages/pages.routes').then(x => x.pagesRoutes)
+    }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [LayoutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
